1a236229b96f31b515d3bf2f9512f206c3a2b7b5
RAJ KUMAR <rk8210032@gmail.com>
2025-08-06 21:35:11 +0530

    link the user with DB

---
+++ journalApp/src/main/java/com/example/journalapp/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.journalapp.controller;
+
+import com.example.journalapp.entity.User;
+import com.example.journalapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User userId) {
+        userService.saveEntry(userId);
+    }
+
+    @PutMapping("/userName")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String username) {
+        User userName = userService.findUser(username);
+        if(userName != null) {
+            userName.setUserName(user.getUserName());
+            userName.setPassword(user.getPassword());
+            userService.saveEntry(userName);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ journalApp/src/main/java/com/example/journalapp/services/UserService.java
@@ -0,0 +1,37 @@
+package com.example.journalapp.services;
+
+import com.example.journalapp.entity.User;
+import com.example.journalapp.repository.UserRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveEntry(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(ObjectId id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id) {
+        userRepository.deleteById(id);
+    }
+
+    public User findUser(String user) {
+        return userRepository.findByUserName(user);
+    }
+}
