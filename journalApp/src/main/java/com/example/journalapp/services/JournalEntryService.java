937cf75d5d52be10646f50e109a8e3c894e1aa3d
RAJ KUMAR <rk8210032@gmail.com>
2025-08-08 20:44:07 +0530

    added @Transaction in this project

--- journalApp/src/main/java/com/example/journalapp/services/JournalEntryService.java
+++ journalApp/src/main/java/com/example/journalapp/services/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.example.journalapp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,12 +21,18 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName) {
-        User user = userService.findUser(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntry().add(saved);
-        userService.saveEntry(user);
+        try {
+            User user = userService.findUser(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntry().add(saved);
+            userService.saveEntry(user);
+        }catch (Exception e) {
+            System.out.print(e.getMessage());
+            throw new RuntimeException("Something uncertain happened in JES saveEntry Method.");
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry) {
