744fd3005c784ab29a1dd1370df3296e9651ebbc
RAJ KUMAR <rk8210032@gmail.com>
2025-08-16 11:12:28 +0530

    learn MongoTemplet, Crieteria

---
+++ journalApp/src/main/java/com/example/journalapp/repository/UserRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.example.journalapp.repository;
+
+import com.example.journalapp.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+public class UserRepositoryImpl {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<User> getUserForSA() {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("email").regex("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$"));
+        query.addCriteria(Criteria.where("sentimentAnalysis").is(true));
+        List<User> users = mongoTemplate.find(query, User.class);
+        return users;
+    }
+}
