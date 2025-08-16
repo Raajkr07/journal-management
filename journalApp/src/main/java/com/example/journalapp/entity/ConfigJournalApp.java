bff6dee92f9c3ee98a0335c56a92ada4c24f2d5d
RAJ KUMAR <rk8210032@gmail.com>
2025-08-16 11:19:49 +0530

    made pojo for application cache

---
+++ journalApp/src/main/java/com/example/journalapp/entity/ConfigJournalApp.java
@@ -0,0 +1,20 @@
+package com.example.journalapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "config_journal_app")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConfigJournalApp {
+    private String key;
+    private String value;
+}
