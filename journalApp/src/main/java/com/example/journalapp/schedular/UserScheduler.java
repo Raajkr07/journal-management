db3534b99be9ad2c7b2cb958e5d0a591dfcf154e
RAJ KUMAR <rk8210032@gmail.com>
2025-08-18 17:12:13 +0530

    wrote logic for mail schedule service

---
+++ journalApp/src/main/java/com/example/journalapp/schedular/UserScheduler.java
@@ -0,0 +1,52 @@
+package com.example.journalapp.schedular;
+
+import com.example.journalapp.entity.JournalEntry;
+import com.example.journalapp.entity.User;
+import com.example.journalapp.enums.Sentiment;
+import com.example.journalapp.repository.UserRepositoryImpl;
+import com.example.journalapp.services.EmailServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class UserScheduler {
+
+    @Autowired
+    private EmailServices emailServices;
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Scheduled(cron = "0 0 * * SUN")
+    public void fetchUserAndSendMail() {
+        List<User> users = userRepository.getUserForSA();
+
+        for (User user : users) {
+            List<JournalEntry> journalEntries = user.getJournalEntry();
+            List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
+            Map<Sentiment, Integer> sentimentCounts = new HashMap<>();
+            for (Sentiment sentiment : sentiments) {
+                if (sentiment != null)
+                    sentimentCounts.put(sentiment, sentimentCounts.getOrDefault(sentiment, 0) + 1);
+            }
+            Sentiment mostFrequentSentiment = null;
+            int maxCount = 0;
+            for (Map.Entry<Sentiment, Integer> entry : sentimentCounts.entrySet()) {
+                if (entry.getValue() > maxCount) {
+                    maxCount = entry.getValue();
+                    mostFrequentSentiment = entry.getKey();
+                }
+            }
+            if (mostFrequentSentiment != null) {
+                emailServices.sendMail(user.getEmail(), "sentiment of previous weak", mostFrequentSentiment.toString());
+            }
+        }
+    }
+}
