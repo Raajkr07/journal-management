299fe8fbc13f139cff28ec1acef3b0f1b9cafe81
RAJ KUMAR <rk8210032@gmail.com>
2025-08-23 19:20:46 +0530

    refractor class name

--- journalApp/src/main/java/com/example/journalapp/services/EmailService.java
+++ journalApp/src/main/java/com/example/journalapp/services/EmailService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
-public class EmailServices {
+public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
