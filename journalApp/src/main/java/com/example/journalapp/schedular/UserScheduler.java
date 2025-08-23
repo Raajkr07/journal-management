d24a6d3d506e951df66ab5ce12c504799e8973a0
RAJ KUMAR <rk8210032@gmail.com>
2025-08-23 19:21:57 +0530

    write kafka codes to implement kafka cloud

--- journalApp/src/main/java/com/example/journalapp/schedular/UserScheduler.java
+++ journalApp/src/main/java/com/example/journalapp/schedular/UserScheduler.java
@@ -1,11 +1,14 @@
 package com.example.journalapp.schedular;
 
+import com.example.journalapp.cache.AppCache;
 import com.example.journalapp.entity.JournalEntry;
 import com.example.journalapp.entity.User;
 import com.example.journalapp.enums.Sentiment;
+import com.example.journalapp.model.SentimentData;
 import com.example.journalapp.repository.UserRepositoryImpl;
-import com.example.journalapp.services.EmailServices;
+import com.example.journalapp.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -20,9 +23,17 @@ import java.util.stream.Collectors;
 public class UserScheduler {
 
     @Autowired
-    private EmailServices emailServices;
+    private EmailService emailService;
+
+    @Autowired
+    private AppCache appCache;
+
     @Autowired
     private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private KafkaTemplate<String, SentimentData> kafkaTemplate;
+
 
     @Scheduled(cron = "0 0 * * SUN")
     public void fetchUserAndSendMail() {
@@ -45,8 +56,18 @@ public class UserScheduler {
                 }
             }
             if (mostFrequentSentiment != null) {
-                emailServices.sendMail(user.getEmail(), "sentiment of previous weak", mostFrequentSentiment.toString());
+                SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days " + mostFrequentSentiment).build();
+                try{
+                    kafkaTemplate.send("weekly-sentiments", sentimentData.getEmail(), sentimentData);
+                }catch (Exception e){
+                    emailService.sendMail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+                }
             }
         }
+    }
+
+    @Scheduled(cron = "0 0/10 * ? * *")
+    public void clearAppCache() {
+        appCache.init();
     }
 }
