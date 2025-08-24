8b03bd3e48b4c4c7d2be873d77d07fe3c566c7f4
RAJ KUMAR <rk8210032@gmail.com>
2025-08-24 14:03:07 +0530

    added @Component anotation

--- journalApp/src/main/java/com/example/journalapp/utils/JwtUtil.java
+++ journalApp/src/main/java/com/example/journalapp/utils/JwtUtil.java
@@ -3,12 +3,14 @@ package com.example.journalapp.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class JwtUtil {
 
     private String SECRET_KEY = "TaK+HaV^uvCHEFsEVfypW#7g9^k*Z8$V";
