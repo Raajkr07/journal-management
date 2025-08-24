e586895dd877fbef5f77c9e7f3d50a3619b88a66
RAJ KUMAR <rk8210032@gmail.com>
2025-08-24 14:00:19 +0530

    add @Service annotation

--- journalApp/src/main/java/com/example/journalapp/services/EmailService.java
+++ journalApp/src/main/java/com/example/journalapp/services/EmailService.java
@@ -3,7 +3,9 @@ package com.example.journalapp.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
 
     @Autowired
