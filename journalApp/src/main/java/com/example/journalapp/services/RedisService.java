0ebfe7b8f2a48f90eb4b000d0b15622203a000ef
RAJ KUMAR <rk8210032@gmail.com>
2026-01-17 23:58:48 +0530

    fix the issues

--- journalApp/src/main/java/com/example/journalapp/services/RedisService.java
+++ journalApp/src/main/java/com/example/journalapp/services/RedisService.java
@@ -1,6 +1,5 @@
 package com.example.journalapp.services;
 
-import com.example.journalapp.apirespone.WeatherResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
